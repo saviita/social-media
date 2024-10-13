abe8149b423e0879232b12c564fe1d4c77968c65
saviita <taatt2000@gmail.com>
2024-10-14 01:00:42 +0200

    +

--- src/constants/followers.js
+++ src/constants/followers.js
@@ -1,20 +1,34 @@
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
 const FOLLOWERS = [
-    {
-      id: v4(),
-      icon: 'x',
-      name: '@vnuv',
-      number: 4534,
-      today: 12
-    },
-    {
-        id: v4(),
-        icon: 'x',
-        name: '@vnuv',
-        number: 4534,
-        today: 12
-      }
-]
+  {
+    id: v4(),
+    icon: "../../public/assets/icon-facebook.svg",
+    name: "@nathanf",
+    number: 1987,
+    today: 12,
+  },
+  {
+    id: v4(),
+    icon: "../../public/assets/icon-twitter.svg",
+    name: "@nathanf",
+    number: 1044,
+    today: 99,
+  },
+  {
+    id: v4(),
+    icon: "../../public/assets/icon-instagram.svg",
+    name: "@realnathanf",
+    number: 11 + "k",
+    today: 1099,
+  },
+  {
+    id: v4(),
+    icon: "../../public/assets/icon-youtube.svg",
+    name: "Nathan F.",
+    number: 8239,
+    today: 144,
+  },
+];
 
-export {FOLLOWERS}
+export { FOLLOWERS };
